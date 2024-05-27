7c08aac430115f15bf6cc0e0f51e8bb082062518
Manisha-Suravaram <107974299+Manisha-Suravaram@users.noreply.github.com>
2024-05-27 21:56:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,20 @@ const server2 = require('./server2');
 const server1 = require('./server1');
 
 const app = express();
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(bodyParser.json());
-app.use(cors())
-app.use('/', server2);
-app.use('/',server1);
 
+// CORS configuration
+const allowedOrigins = ['http://localhost:3000', 'https://rentify-task.vercel.app'];
+app.use(cors({
+    origin: allowedOrigins,
+    methods: ['GET', 'POST'],
+    credentials: true,
+}));
+
+app.use('/', server2);
+app.use('/', server1);
 
 app.listen(8000, () => {
     console.log('Server started on port: 8000');
