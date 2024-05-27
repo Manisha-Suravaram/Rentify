5b87a9fc5410a519b5999a3d5887c2d5637c9a21
Manisha-Suravaram <107974299+Manisha-Suravaram@users.noreply.github.com>
2024-05-27 21:56:45 +0530

    Update server1.js

--- backend/server1.js
+++ backend/server1.js
@@ -1,51 +1,56 @@
 const express = require('express');
 const cors = require('cors');
-const connection = require('./Connection')
+const connection = require('./Connection');
 const col_users = require('./databases/login');
 const col_properties = require('./databases/property');
 
-const app = express()
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
-app.post('/signup',(req,res) => {
-    console.log(req.body)
+// CORS configuration
+const allowedOrigins = ['http://localhost:3000', 'https://rentify-task.vercel.app'];
+app.use(cors({
+    origin: allowedOrigins,
+    methods: ['GET', 'POST'],
+    credentials: true,
+}));
+
+app.post('/signup', (req, res) => {
+    console.log(req.body);
     col_users.insertMany({
-        firstname:req.body.fname,
-        lastname:req.body.lname,
-        email:req.body.email,
-        mobile:req.body.phone, 
-        role:req.body.role,
-        password:req.body.pwd
+        firstname: req.body.fname,
+        lastname: req.body.lname,
+        email: req.body.email,
+        mobile: req.body.phone,
+        role: req.body.role,
+        password: req.body.pwd,
     }).then((res1) => {
-        if(res1.length != 0){
-            res.send({valid:true})
-        }  
-        else{
-            res.send({valid:false})
-        } 
-    })
-}) 
+        if (res1.length != 0) {
+            res.send({valid: true});
+        } else {
+            res.send({valid: false});
+        }
+    });
+});
 
-app.post('/login',(req,res) => {
+app.post('/login', (req, res) => {
     console.log(req.body);
     col_users.find({
         email: req.body.uname,
-        password: req.body.pwd
+        password: req.body.pwd,
     })
     .then(res2 => {
         console.log(res2);
-        if(res2.length !== 0){
-            res.send({role:res2[0].role,mail:res2[0].email,valid:true})
-        }else{
-            res.send({role:"invalid",valid:false})
+        if (res2.length !== 0) {
+            res.send({role: res2[0].role, mail: res2[0].email, valid: true});
+        } else {
+            res.send({role: 'invalid', valid: false});
         }
     })
     .catch(err => {
         console.log(err);
-    })
-})
+    });
+});
 
-
-module.exports = app 
+module.exports = app;
